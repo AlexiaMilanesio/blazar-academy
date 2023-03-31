9a06ea5d0a9a4e3bc5a0c009122a88d569434fe4
AlexiaMilanesio <alexiamilanesio9@gmail.com>
2023-03-31 14:17:26 +0200

    es. climbing the leaderboard medium difficulty

--- lezioni-frontend/lezione js/hackerrank.js
+++ lezioni-frontend/lezione js/hackerrank.js
@@ -1,17 +1,12 @@
 // *********************** HACKERRANK ************************** //
 
+
+// ********************** EASY DIFFICULTY ********************** //
+
 // ************************** ES 1 ***************************** //
 
 
-/*
- * Complete the 'reverseArray' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts INTEGER_ARRAY a as parameter.
- */
-
 function reverseArray(a) {
-    // Write your code here
     return a.reverse();
 
     // let reversedArray = [];
@@ -26,19 +21,10 @@ function reverseArray(a) {
 //        so we go from back to front pushing every integer on the original array into the new array
 
 
-
 // ************************** ES 2 ***************************** //
 
 
-/*
- * Complete the 'simpleArraySum' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts INTEGER_ARRAY ar as parameter.
- */
-
 function simpleArraySum(ar) {
-    // Write your code here
     return ar.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 }
 
@@ -46,14 +32,7 @@ function simpleArraySum(ar) {
 // ************************** ES 3 ***************************** //
 
 
-/*
- * Complete the 'staircase' function below.
- *
- * The function accepts INTEGER n as parameter.
- */
-
 function staircase(n) {
-    // Write your code here
     // for (let i = 0; i < n; i++) {              // while i is smaller than n or smaller or equal to n - 1
     //     let staircase = "";
 
@@ -95,22 +74,10 @@ function staircase(n) {
 //         --->  # = i+1   | # = 4 + 1 | # = 5 
 
 
-
 // ************************** ES 4 ***************************** //
 
 
-/*
- * Complete the 'divisibleSumPairs' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts following parameters:
- *  1. INTEGER n
- *  2. INTEGER k
- *  3. INTEGER_ARRAY ar
- */
-
 function divisibleSumPairs(n, k, ar) {
-    // Write your code here
     let counter = 0;
 
     for (let i = 0; i < n; i++) {
@@ -128,22 +95,7 @@ function divisibleSumPairs(n, k, ar) {
 // ************************** ES 5 ***************************** //
 
 
-/*
- * Complete the 'compareTriplets' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts following parameters:
- *  1. INTEGER_ARRAY a
- *  2. INTEGER_ARRAY b
- * 
- * 
- * If a[i] > b[i], then person a is awarded 1 point.
- * If a[i] < b[i], then person b is awarded 1 point.
- * If a[i] = b[i], then neither person receives a point.
- */
-
 function compareTriplets(a, b) {
-    // Write your code here
     let score = [0, 0];
 
     for (let i = 0; i < a.length; i++) {
@@ -161,15 +113,7 @@ function compareTriplets(a, b) {
 // ************************** ES 6 ***************************** //
 
 
-/*
- * Complete the 'hourglassSum' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts 2D_INTEGER_ARRAY arr as parameter.
- */
-
 function hourglassSum(arr) {
-    // Write your code here
     const sumHourglass = (row, col) => {                 // Sum the corresponding "coordinates" within the matrix
         return arr[row][col] + arr[row][col+1] + arr[row][col+2] + 
                arr[row+1][col+1] +
@@ -190,101 +134,43 @@ function hourglassSum(arr) {
 }
 
 
-// **************** EXTRA: MEDIUM DIFFICULTY ******************* //
-// **************** Climbing the leaderboard ******************* //
 
-/*
- * Complete the 'climbingLeaderboard' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts following parameters:
- *  1. INTEGER_ARRAY ranked
- *  2. INTEGER_ARRAY player
- */
+// ********************* MEDIUM DIFFICULTY ********************* //
+
+// **************** Climbing the leaderboard ******************* //
 
 
 function climbingLeaderboard(ranked, player) {
-    let rankingNumbers = [];                                                // Leaderboard ranking positions/numbers of a player
-    let scoresSet = [...new Set(ranked)];                                   // Not repeated leaderboard scores
-    
-
-    console.log(ranked);
-    console.log(scoresSet);
-    console.log(player);
-
+    let rankingNumbers = [];                                                
+    let scoresSet = [...new Set(ranked)];  
+    let index = scoresSet.length -1;
 
     for (const score of player) {
+
+        console.log(index);
+        console.log(scoresSet[index]);
         console.log(score);
-        if (score >= scoresSet[0]) {                                        // Player's score is in the first position of the leaderboard
-            console.log(score);
-            rankingNumbers.push(1);                                         
-        }
-        else if (score < scoresSet[scoresSet.length - 1]) {                 // Player's score is in the last position of the leaderboard
-            console.log(score);
-            rankingNumbers.push(scoresSet.length + 1);
-        } 
-        else {
-            console.log(score);
-            rankingNumbers.push(calcMiddleRanking(score, scoresSet));       // Player's score is in the middle positions of the leaderboard}
-        }
-
         console.log(rankingNumbers);
-    }
 
+        while (index >= 0) {
+            // While player's score is higher or equal to the score on the current position of the leaderboard, decrement position in 1
+            if (score >= scoresSet[index]) index--;
+
+            // If player's score is lower than the score on the current position of the leaderboard, push the position to the rankingNumbers array
+            // +2 because 1) index is not the same as position (+1) and 2) we have a number that repeats 1 time (+1) 
+            else {
+                rankingNumbers.push(index + 2);  
+                break; 
+            }
+        }
+        if (index < 0) rankingNumbers.push(1);
+    }
     return rankingNumbers;
 }
 
-function calcMiddleRanking(score, scoresSet) {
-    let firstP = 0;
-    let lastP = scoresSet.length - 1;
-        
-    while (true) {
-        // Calculate amount of players to calculate the *middle position number*
-        let middleP = Math.floor((firstP + lastP) / 2);
 
 
-        console.log(middleP);
-        console.log(score);
-        
-
-        // 1. Player's score is equal to middle score of the leaderboard 
-        // --> position players's score 1 position down from the middle of the leaderboard
-        if (scoresSet[middleP] === score) {
-            console.log(middleP);
-            console.log(score);
-            
-            return middleP + 1;
-        }
-
-        // 2. Player's score is lower than middle score AND higher than the score below (middleP+1)
-        // --> position player's score 2 positions down from the middle of the leaderboard
-        else if (scoresSet[middleP] > score && scoresSet[middleP+1] < score) {
-            console.log(middleP);
-            console.log(score);
-
-            return middleP + 2;
-        }
-        
-        // 3. Player's score is higher than middle score AND lower than the score above (middleP-1)
-        // --> position player's score 1 position above from the middle of the leaderboard
-        else if (scoresSet[middleP] < score && scoresSet[middleP-1] > score) {
-            console.log(middleP);
-            console.log(score);
-
-            return middleP - 1;
-        }
-        
-
-        // 4.  *RECURSION*: Player's score is lower or higher than the existing scores on the leaderboard 
-        // 4a. If player's score is lower than the score in the middle position, now the first position 
-        //     will be the middleP+1 (first score of the leaderboard's second half where the lower scores are)
-        if (score < scoresSet[middleP]) firstP = middleP + 1;
-
-        // 4b. If player's score is higher than the score in the middle position, now the last position 
-        //     will be the middleP-1 (last score of the leaderboard's first half where the lower scores are)
-        else lastP = middleP - 1;
-    }
-}
+// ************************** TESTS *************************** //
 
 let ranked1 = [100, 90, 90, 80, 75, 60];
 let player1 = [50, 65, 77, 90, 102];
@@ -302,7 +188,7 @@ console.log(results1);
 // // rankingNumbers = [ 6, 4, 3, 2, 1 ];
 
 
-// let ranked3 = [100, 90, 80, 50, 50, 40];
+// let ranked3 = [100, 90, 80, 50, 40, 40];
 // let player3 = [30, 45, 75, 95, 105];
 
 // const results3 = climbingLeaderboard(ranked3, player3);
